5b71345cc3f33628eab208bab3fddd28ee0376e6
harshal-77 <98039213+harshal-77@users.noreply.github.com>
2026-06-11 14:29:33 +0000

    Added variable Js

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144552;
+let accountEmail = "champoin@mgmail.com"
+var accountpaswwrod = "2324"
+accountCity = "Mumbai"
+let accountState;
+
+// accountId = 123;
+accountEmail = "lsjfl";
+accountpaswwrod = "2342";
+accountCity = "Jaipur"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountState,accountpaswwrod,accountCity])

227f969d3fc8eec2efe286df73bb8a896b23679c
harshal-77 <98039213+harshal-77@users.noreply.github.com>
2026-06-11 15:30:51 +0000

    Conversion and Operations

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,45 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "22" = 22
+// "22123" = NaN
+// true = 1; false = 0;
+
+
+let isLoggedIn = "champion"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+// console.log(typeof stringNumber);
+
+
+/****************** Operations **********************************/
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log("1" + 2  + 2);
+// console.log(1 + 2  + "2");
+// console.log(2 + "1");
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);

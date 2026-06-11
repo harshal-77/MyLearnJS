4ff3bfe413eb0d9d08d0fc89e8666c52b089200d
harshal-77 <98039213+harshal-77@users.noreply.github.com>
2026-06-11 14:54:11 +0000

    Datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+
+// number = 2 to power 53
+// bigint
+// string
+// boolean
+// null : standalone value
+// undefined
+// symbol : unique
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof null);
+console.log(typeof undefined);

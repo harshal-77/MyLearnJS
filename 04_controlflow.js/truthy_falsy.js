3b26fca6e07f851fd0269ccfcaae52903ceced11
harshal-77 <98039213+harshal-77@users.noreply.github.com>
2026-06-14 11:13:58 +0000

    control flow

---
+++ 04_controlflow.js/truthy_falsy.js
@@ -0,0 +1,22 @@
+// falsy
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// Truthy
+
+// "0", 'false', " ", [], {}, function(){}
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("Object is empty")
+}
+
+// Nullish coalescing operator ??: null undefined  
+
+// let val1 = null ?? 10
+// val1 = undefined ?? 15
+
+const price = 100;
+
+price >= 100 ? console.log("true") : console.log("false")

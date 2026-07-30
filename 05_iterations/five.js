a0cc9d3a8dacc56c7c6e44ce8466922b289cedc0
harshal-77 <98039213+harshal-77@users.noreply.github.com>
2026-07-30 17:01:38 +0000

    iterations

---
+++ 05_iterations/five.js
@@ -0,0 +1,42 @@
+const coding = ["fs",'dsf','sd']
+
+// coding.forEach(function(val){
+//     console.log(val);
+    
+// })
+
+
+// coding.forEach((val)=>{
+//     console.log(val)
+// })
+
+// function printMe(item){
+//     console.log(item)
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach((item,index,arr) => {
+//     console.log(item,index,arr)
+// })
+
+
+const myCoding = [
+    {
+        langname: "js",
+        filename:"jss"
+    },
+    {
+        langname: "java",
+        filename:"java"
+    },
+    {
+        langname: "python",
+        filename:"py"
+    }
+]
+
+
+myCoding.forEach((item)=>{
+    console.log(item.filename)
+})
